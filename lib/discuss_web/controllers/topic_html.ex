8d612c7170372dab3861b48e05b7da1f2ed6eaee
Shrouk-Abdullah <95624488+Shrouk-Abdullah@users.noreply.github.com>
2024-01-19 23:13:55 +0200

    Websocket

--- lib/discuss_web/controllers/topic_html.ex
+++ lib/discuss_web/controllers/topic_html.ex
@@ -1,20 +1,20 @@
 defmodule DiscussWeb.TopicHTML do
   use DiscussWeb, :html
 
-  import Phoenix.HTML.Form
-  use Phoenix.Router
+  # import Phoenix.HTML.Form
+  # use Phoenix.Router
 
-  def topic_path(conn, :create) do
-    DiscussWeb.Router.Helpers.topic_path(conn, :create)
-  end
+  # def topic_path(conn, :create) do
+  #   DiscussWeb.Router.Helpers.topic_path(conn, :create)
+  # end
 
-  def topic_path(conn, :new) do
-    DiscussWeb.Router.Helpers.topic_path(conn, :new)
-  end
+  # def topic_path(conn, :new) do
+  #   DiscussWeb.Router.Helpers.topic_path(conn, :new)
+  # end
 
-  def topic_path(conn, :update, topic) do
-    DiscussWeb.Router.Helpers.topic_path(conn, :update, topic)
-  end
+  # def topic_path(conn, :update, topic) do
+  #   DiscussWeb.Router.Helpers.topic_path(conn, :update, topic)
+  # end
 
   embed_templates "topic_html/*"
 end
